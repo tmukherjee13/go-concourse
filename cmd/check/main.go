e5b32761e7f28aa8279be71a616aff5e09067452
Tarun M <tmukherjee13@gmail.com>
2021-12-13 12:01:24 +0530

    Updated  check

--- cmd/check/main.go
+++ cmd/check/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world. from  check")
+
+	bytes, err := ioutil.ReadAll(os.Stdin)
+
+	if err != nil {
+		log.Fatal("Error reading from stdin: ", err)
+	}
+	fmt.Printf("%s", bytes)
+
+	versions := []string{"HEAD"}
+	err = json.NewEncoder(os.Stdout).Encode(versions)
+	if err != nil {
+		log.Fatalf("encoding error: %s", err)
+	}
 }
