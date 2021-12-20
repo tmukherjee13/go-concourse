39a1be1aeb1eb9637e515fa2e5aa6355a80ed77a
Tarun M <tmukherjee13@gmail.com>
2021-12-20 22:45:09 +0530

    updated  in script

--- cmd/in/main.go
+++ cmd/in/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world. from IN")
+	bytes, err := ioutil.ReadAll(os.Stdin)
+
+	if err != nil {
+		log.Fatal("Error reading from stdin: ", err)
+	}
+	fmt.Printf("%s", bytes)
+
+	versions := []string{"https://github.com/tmukherjee13/go-concourse", "https://github.com/tmukherjee13/yii2-reverse-migration"}
+	err = json.NewEncoder(os.Stdout).Encode(versions)
+	if err != nil {
+		log.Fatalf("encoding error: %s", err)
+	}
 }
